876a2dc31858502306a3a0dee1eccfe6b9fde7a9
sambodhikamble28 <sambodhikamble8@gmail.com>
2025-08-22 20:17:14 +0530

    reverse the given string

---
+++ ReverseWords.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String s = sc.nextLine();
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) result.append(" "); 
+        }
+        System.out.println("Reversed string: " + result.toString());
+    }
+}
