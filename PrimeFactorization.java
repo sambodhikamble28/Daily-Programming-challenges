bb19e4038f9241450c05aeb8c5b9d3ac5874393b
sambodhikamble28 <sambodhikamble8@gmail.com>
2025-08-31 21:37:04 +0530

    PrimeFactorization.java

---
+++ PrimeFactorization.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class PrimeFactorization {
+    public static List<Integer> primeFactors(int n) {
+        List<Integer> factors = new ArrayList<>();
+        while (n % 2 == 0) {
+            factors.add(2);
+            n /= 2;
+        }
+
+        for (int i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 2) {
+            factors.add(n);
+        }
+
+        return factors;
+    }
+
+    public static void main(String[] args) {
+        int N = 18;
+        System.out.println(primeFactors(N));
+    }
+}
