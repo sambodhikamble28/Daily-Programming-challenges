cf3b55cc4594aeae399c98958f8879ad56ad915d
sambodhikamble28 <sambodhikamble8@gmail.com>
2025-09-12 19:16:57 +0530

    Create FibonacciOptimized.java

---
+++ FibonacciOptimized.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FibonacciOptimized {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        long a = 0, b = 1, c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        System.out.println(c);
+    }
+}
